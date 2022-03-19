24b92e2cafc0df8a54caaad47bdb7e4714937872
Nathan Ganser <47492950+nathanganser@users.noreply.github.com>
2022-03-19 16:13:54 -0700

    basic app

---
+++ application.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+
+application = Flask(__name__)
+
+secret = "OK"  # can you access this data
+
+
+@application.route("/execute")
+def execute():
+    code = request.json.get('code')
+
+    response = {}
+    try:
+        exec(code, {"__builtins__": None}, response)
+    except Exception as e:
+        print(e)
+        return jsonify("Error: " + str(e))
+    print("done")
+    return jsonify(response)
+
+
+application.run()
---
+++ play.py
@@ -0,0 +1,3 @@
+data = {"a": 33} # can you get this data?
+
+print(data.get("a")) # can you execute this function?
